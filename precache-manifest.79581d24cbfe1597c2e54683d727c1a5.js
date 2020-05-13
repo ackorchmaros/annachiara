64ecb13effff7f429f5d55a9db4cf16350b68c88
matteo <matteo@havenlife.com>
2020-05-13 15:15:15 +0200

    Updates

---
+++ precache-manifest.79581d24cbfe1597c2e54683d727c1a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30571ec45965bc1b56e4cab528ff80cb",
+    "url": "/matteozancanella/index.html"
+  },
+  {
+    "revision": "fd4b0d25e43d09c8c9dc",
+    "url": "/matteozancanella/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ce571bc2cca99456e5d0",
+    "url": "/matteozancanella/static/js/2.82d857d4.chunk.js"
+  },
+  {
+    "revision": "fd4b0d25e43d09c8c9dc",
+    "url": "/matteozancanella/static/js/main.41fc9bbc.chunk.js"
+  },
+  {
+    "revision": "469a9f8fcf0888bc99c3",
+    "url": "/matteozancanella/static/js/runtime~main.1700ec7e.js"
+  }
+]);
